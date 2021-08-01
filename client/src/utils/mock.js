2bd1092703966232f5827165598c869c564114b9
wenhaofang <wenhaofang1998@qq.com>
2021-08-01 16:28:45 +0800

    fix[client]: util mockjs precise query

--- client/src/utils/mock.js
+++ client/src/utils/mock.js
@@ -1,38 +1,47 @@
-function getKGData () {
-  const nodes = [{
-    id: 'n1',
-    name: '李白',
-    label: '人物'
-  }, {
-    id: 'n2',
-    name: '杜甫',
-    label: '人物'
-  }, {
-    id: 'n3',
-    name: '《静夜思》',
-    label: '作品'
-  }, {
-    id: 'n4',
-    name: '《茅屋为秋风所破歌》',
-    label: '作品'
-  }]
-  const edges = [{
-    id: 'e1',
-    source: 'n1',
-    target: 'n2',
-    relationship: '李杜'
-  }, {
-    id: 'e2',
-    source: 'n1',
-    target: 'n3',
-    relationship: '作品'
-  }, {
-    id: 'e3',
-    source: 'n2',
-    target: 'n4',
-    relationship: '作品'
-  }]
-  return [nodes, edges]
+const nodes = [{
+  id: 'n1',
+  name: '李白',
+  label: '人物'
+}, {
+  id: 'n2',
+  name: '杜甫',
+  label: '人物'
+}, {
+  id: 'n3',
+  name: '静夜思',
+  label: '诗歌'
+}, {
+  id: 'n4',
+  name: '茅屋为秋风所破歌',
+  label: '诗歌'
+}]
+
+const edges = [{
+  id: 'e1',
+  source: 'n1',
+  target: 'n2',
+  relationship: '李杜'
+}, {
+  id: 'e2',
+  source: 'n1',
+  target: 'n3',
+  relationship: '作品'
+}, {
+  id: 'e3',
+  source: 'n2',
+  target: 'n4',
+  relationship: '作品'
+}]
+
+async function getKGData (query) {
+  const queryNode = nodes.filter(node => node.name === query)[0]
+  const queryNodeId = queryNode && queryNode.id
+  const selectedEdges = edges.filter(edge => edge.source === queryNodeId || edge.target === queryNodeId)
+  const sourceNodeIds = selectedEdges.map(edge => edge.source)
+  const targetNodeIds = selectedEdges.map(edge => edge.target)
+  const selectedNodeIds = [...sourceNodeIds, ...targetNodeIds]
+  const selectedNodes = nodes.filter(node => selectedNodeIds.includes(node.id))
+  return [selectedNodes, selectedEdges]
 }
 
 export {
